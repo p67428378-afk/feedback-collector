cbd3264e3f2f68857212fe649b42c3989e64113d
p67428378-afk <p67428378@gmail.com>
2026-03-06 05:01:49 +0000

    SCRUM-27: Add frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+import FormBuilder from './pages/FormBuilder';
+import UserFormPage from './pages/UserFormPage';
+import SubmissionsPage from './pages/SubmissionsPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/admin">Admin Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/admin/forms/new">Create New Form</Link>
+            </li>
+            {/* Link to a sample user form - replace with dynamic links later */}
+            <li>
+              <Link to="/form/1">Sample User Form (ID 1)</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/forms/new" element={<FormBuilder />} />
+          <Route path="/admin/forms/:formId/edit" element={<FormBuilder />} />
+          <Route path="/admin/forms/:formId/submissions" element={<SubmissionsPage />} />
+          <Route path="/form/:formId" element={<UserFormPage />} />
+          <Route path="/" element={<h1>Welcome to the Anonymous Feedback System</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
