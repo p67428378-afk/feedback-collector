9c59723c6c97602cfe64bb79eb4a65fabecae25c
p67428378-afk <p67428378@gmail.com>
2026-03-06 05:18:16 +0000

    SCRUM-26: Frontend: Integrate AllSubmissionsDashboard into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import AdminDashboard from './pages/AdminDashboard';
 import FormBuilder from './pages/FormBuilder';
 import UserFormPage from './pages/UserFormPage';
 import SubmissionsPage from './pages/SubmissionsPage';
+import AllSubmissionsDashboard from './pages/AllSubmissionsDashboard'; // New import
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
             <li>
               <Link to="/admin/forms/new">Create New Form</Link>
             </li>
+            <li>
+              <Link to="/admin/submissions/all">All Submissions</Link> {/* New Link */}
+            </li>
             {/* Link to a sample user form - replace with dynamic links later */}
             <li>
               <Link to="/form/1">Sample User Form (ID 1)</Link>
@@ -30,6 +34,7 @@ function App() {
           <Route path="/admin/forms/new" element={<FormBuilder />} />
           <Route path="/admin/forms/:formId/edit" element={<FormBuilder />} />
           <Route path="/admin/forms/:formId/submissions" element={<SubmissionsPage />} />
+          <Route path="/admin/submissions/all" element={<AllSubmissionsDashboard />} /> {/* New Route */}
           <Route path="/form/:formId" element={<UserFormPage />} />
           <Route path="/" element={<h1>Welcome to the Anonymous Feedback System</h1>} />
         </Routes>
